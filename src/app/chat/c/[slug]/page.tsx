4dc5b6e1a2c166edf977711b86f178a90295e54c
rasha <aysharasha258@gmail.com>
2025-08-31 18:09:14 +0530

    type error

--- src/app/chat/c/[slug]/page.tsx
+++ src/app/chat/c/[slug]/page.tsx
@@ -1,28 +1,22 @@
+"use client"
+
 import ChatBox from "@/components/chat/chatBox"
-import UserHeader from "@/components/user/userHeader"
-import { cookies } from 'next/headers';
+import Cookies from "js-cookie"
 
 interface ChatRoomProps {
-    params: {
-        slug: string;
-    };
+  params: {
+    slug: string
+  }
 }
-export default async function ChatRoom({ params }:ChatRoomProps) {
-    
-    const { slug } =  params;
-    
-   
-    const cookieStore = await cookies();
-    const authToken = cookieStore.get("authToken")?.value || null;
-    
-    return(
-        <>
-       
-       <div className="flex h-screen">
-            
-                <ChatBox chatId={slug} id={authToken }/>
-            </div>
-        </>
-        
-    )
- };
+
+export default function ChatRoom({ params }: ChatRoomProps) {
+  const { slug } = params
+  
+  const authToken = Cookies.get("authToken") ?? null
+
+  return (
+    <div className="flex h-screen">
+      <ChatBox chatId={slug} id={authToken} />
+    </div>
+  )
+}
