e371e2341dcbace451852edd839c55fc77379619
rasha <aysharasha258@gmail.com>
2025-09-01 13:55:33 +0530

    logout domain

--- src/app/lib/server/logout.tsx
+++ src/app/lib/server/logout.tsx
@@ -5,15 +5,14 @@ import { cookies } from 'next/headers';
 export async function logoutUser() {
   const cookieStore = await cookies();
 
-  // Get all cookies
+
   const allCookies = cookieStore.getAll();
   const isProduction:boolean = process.env.NODE_ENV === "production"
-  // Iterate through each cookie and delete it
   for (const cookie of allCookies) {
     cookieStore.delete({
       name : cookie.name,  
       path: '/', 
-      domain:'app.levelup.icu',
+      domain:'.levelup.icu',
       secure: true, 
       sameSite: isProduction ?'none' : 'lax'
   });
